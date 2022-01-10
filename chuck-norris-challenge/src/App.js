eca373696bbf7dc5817a87b74451ad10e582786c
angelangelov88 <82048582+angelangelov88@users.noreply.github.com>
2022-01-10 19:14:35 +0000

    Added the CSS and React states
    - I started with adding some Sass to the page in order to implement the first part of the task
    - I added header and made it fixed
    - I added left sidebar with not text
    - I gave background colours to both of them in order to make sure they are visible and working as intended
    - I made sure the main content can be scrolled
    - I added a breakpoint for small screens where the sidebar is not visible. I used display: none to remove it from small screens
    - I started the React part by fetching the API response to the screen. I used Axios for that
    - After that I added the buttons
    - The Get a new joke button is assigned to a function which sends a request to the API endpoint and returns an array with the response.  I initiated the state with an empty array.  keep the response in a joke state.
    - I have also added another state hook for pointer which counts where the user is currently. This pointer starts at 0 and is being set to the joke.length when the fetch function is invoked. That way it shows the correct statement to the user
    - I added an if statement to have the PREV button shown only if there are more than 1 items in the array
    - I also get rid of the NEXT button when this same function is invoked again (as the user will see the last item in the array)
    - After that I added 2 functions for the buttons - prevJoke() and nextJoke(). I have attached them to the buttons with the onClick event
    - Those functions increment/decrement the pointer value depending on the user click.
    - I do change the button state (visible or not) when the user reaches the last element

--- chuck-norris-challenge/src/App.js
+++ chuck-norris-challenge/src/App.js
@@ -1,23 +1,78 @@
-import logo from './logo.svg';
 import './App.scss';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [joke, setJoke] = useState([''])
+  const [pointer, setPointer] = useState(0)
+  const [prevBtnShown, setPrevBtnShown] = useState(false)
+  const [nextBtnShown, setNextBtnShown] = useState(false)
+
+  useEffect(  () => {
+    newJoke();
+  }, []);
+
+  const newJoke = async () => {
+    const result = await axios.get('https://api.chucknorris.io/jokes/random');
+    setJoke([...joke, result.data.value]);
+    console.log(joke)
+    if (joke.length > 1) {
+      setPrevBtnShown(true);
+  }
+    setPointer(joke.length)
+    setNextBtnShown(false)
+  }
+
+  const prevJoke = () => {
+    if (pointer <= 1 ) {
+      setPrevBtnShown(false)
+    } else {
+      setPointer(pointer -1)
+      setNextBtnShown(true)
+    }
+    console.log(pointer)
+  }
+
+  const nextJoke = () => {
+    if (pointer == joke.length -1 ) {
+      setNextBtnShown(false)
+    } else {
+      setPointer(pointer +1)
+      setPrevBtnShown(true)
+    }
+    console.log(pointer)
+
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Chuck Norris App</h1>
       </header>
+      <nav className="App-nav">
+        <ul>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </nav>
+      <main className="App-main">
+        <div className='joke-container'>
+          <h2>Chuck Norris Joke:</h2>
+          <h3>{ joke[pointer] }</h3>
+        </div>
+        <div className="buttons">
+          { prevBtnShown && 
+            <button onClick={prevJoke}>Previous joke</button>
+          }
+          <button onClick={newJoke} className='newJoke'>Get a new joke</button>
+          { nextBtnShown && 
+            <button onClick={nextJoke}>Next joke</button>
+          }
+        </div>
+      </main>
     </div>
   );
 }
