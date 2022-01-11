0fdad6f2d2e9a172c469f0fe61584312ef3c2a5f
angelangelov88 <82048582+angelangelov88@users.noreply.github.com>
2022-01-11 09:20:56 +0000

    Fixed the issue with the counter/pointer
    I had an issue that the pointer did count correctly but the new state did not transfer to the prev and next functions accordingly and another click on the buttons was needed despite being on the last item in the array.
    I decided to take the if statements out of the function and add an useEffect hook in order to make the pointer count correctly the jokes. I added a few if statements to make sure that all eventualities are covered and gave pointer and joke.length as dependencies
    I will do a few more tests but it seems that everything is working
    I also added comments to make sure that my code is easily understandable

--- chuck-norris-challenge/src/App.js
+++ chuck-norris-challenge/src/App.js
@@ -1,51 +1,66 @@
-import './App.scss';
+import './App.css';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 
 function App() {
-  const [joke, setJoke] = useState([''])
+  //I added the useState hooks in order to be able to change the states of those variables in the app
+  const [joke, setJoke] = useState([])
   const [pointer, setPointer] = useState(0)
   const [prevBtnShown, setPrevBtnShown] = useState(false)
   const [nextBtnShown, setNextBtnShown] = useState(false)
 
+  // I added the useEffect hook to make sure a joke is loaded on page load
   useEffect(  () => {
     newJoke();
   }, []);
 
+  useEffect(() => {
+    if (joke.length > 0) {
+      setPointer(joke.length - 1)
+
+    }
+  }, [joke]);
+
+  //I had to move the if statements to a useEffect hook as the pointer did not count correctly if in the function itself. I have added if statement to make sure all possible scenarios are covered and the buttons are shown/hidden accordingly
+  useEffect(() => {
+    if (pointer <= 0) {
+      setPrevBtnShown(false);
+    }
+    if (pointer > 0) {
+      setPrevBtnShown(true);
+    }
+    if (pointer >= 0 && pointer !== joke.length -1) {
+      setNextBtnShown(true);
+    }
+    if (pointer === joke.length -1) {
+      setNextBtnShown(false);
+    } 
+  }, [pointer, joke.length]);
+
+  //This is the fetch function to get a joke from the API endpoint. I use it firstly on load and then on click on Get a new joke button
   const newJoke = async () => {
     const result = await axios.get('https://api.chucknorris.io/jokes/random');
     setJoke([...joke, result.data.value]);
-    console.log(joke)
-    if (joke.length > 1) {
-      setPrevBtnShown(true);
-  }
-    setPointer(joke.length)
     setNextBtnShown(false)
+    console.log(joke)
+    if (joke.length > 0) {
+      setPrevBtnShown(true);
+    }
   }
-
+  
+  //This is the function to decrement the pointer value and show the previous joke
   const prevJoke = () => {
-    if (pointer <= 1 ) {
-      setPrevBtnShown(false)
-    } else {
       setPointer(pointer -1)
-      setNextBtnShown(true)
-    }
-    console.log(pointer)
   }
 
+  //This is the function to increment the pointer value and show the next joke
   const nextJoke = () => {
-    if (pointer == joke.length -1 ) {
-      setNextBtnShown(false)
-    } else {
       setPointer(pointer +1)
-      setPrevBtnShown(true)
-    }
-    console.log(pointer)
-
   }
 
   return (
     <div className="App">
+{/* I have added a header and navbar as per the guidelines and styled them to be visible. The sidebar gets hidden on small screens */}
       <header className="App-header">
         <h1>Chuck Norris App</h1>
       </header>
@@ -59,10 +74,12 @@ function App() {
         </ul>
       </nav>
       <main className="App-main">
+{/* This is the main container of the joke. Depending on what is the pointer value, I show the joke accordingly */}
         <div className='joke-container'>
           <h2>Chuck Norris Joke:</h2>
           <h3>{ joke[pointer] }</h3>
         </div>
+{/* These are the buttons. I have styled the Get a new joke a bit differently and it is always visible. The other 2 get hidden when not needed (for example Previous is hidden when on the first item in the jokes array) */}
         <div className="buttons">
           { prevBtnShown && 
             <button onClick={prevJoke}>Previous joke</button>
